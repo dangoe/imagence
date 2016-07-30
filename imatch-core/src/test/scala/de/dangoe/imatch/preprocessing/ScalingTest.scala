ee5c38d31738b36544156d691eab6d00ca843593
Daniel Goetten <daniel.goetten@googlemail.com>
2016-07-30 14:29:11 +0200

    Testcases added.

--- imatch-core/src/test/scala/de/dangoe/imatch/preprocessing/ScalingTest.scala
+++ imatch-core/src/test/scala/de/dangoe/imatch/preprocessing/ScalingTest.scala
@@ -32,31 +32,32 @@ import org.scalatest.{Matchers, WordSpec}
   */
 class ScalingTest extends WordSpec with Matchers {
 
-  val bounds = Dimension(40, 40)
+  val downscalingBounds = Dimension(40, 40)
+  val upscalingBounds = Dimension(200, 200)
 
   val landscapeFormatImage = new BufferedImage(160, 120, BufferedImage.TYPE_BYTE_GRAY)
   val portraitFormatImage = new BufferedImage(120, 160, BufferedImage.TYPE_BYTE_GRAY)
   val squareFormatImage = new BufferedImage(120, 120, BufferedImage.TYPE_BYTE_GRAY)
 
   "Scaling" should {
-    "scale an image to defined width" when {
+    "scale an image down to defined width" when {
       "to width scaling is used" when {
         "image is in landscape format." in {
-          val resized = new Scaling(bounds, ToWidth).apply(landscapeFormatImage)
+          val resized = new Scaling(downscalingBounds, ToWidth).apply(landscapeFormatImage)
 
           resized.getWidth shouldBe 40
           resized.getHeight shouldBe 30
         }
 
         "image is in portrait format." in {
-          val resized = new Scaling(bounds, ToWidth).apply(portraitFormatImage)
+          val resized = new Scaling(downscalingBounds, ToWidth).apply(portraitFormatImage)
 
           resized.getWidth shouldBe 40
           resized.getHeight shouldBe 53
         }
 
         "image is in square format." in {
-          val resized = new Scaling(bounds, ToWidth).apply(squareFormatImage)
+          val resized = new Scaling(downscalingBounds, ToWidth).apply(squareFormatImage)
 
           resized.getWidth shouldBe 40
           resized.getHeight shouldBe 40
@@ -64,24 +65,24 @@ class ScalingTest extends WordSpec with Matchers {
       }
     }
 
-    "scale an image to defined height" when {
+    "scale an image down to defined height" when {
       "to height scaling is used" when {
         "image is in landscape format." in {
-          val resized = new Scaling(bounds, ToHeight).apply(landscapeFormatImage)
+          val resized = new Scaling(downscalingBounds, ToHeight).apply(landscapeFormatImage)
 
           resized.getWidth shouldBe 53
           resized.getHeight shouldBe 40
         }
 
         "image is in portrait format." in {
-          val resized = new Scaling(bounds, ToHeight).apply(portraitFormatImage)
+          val resized = new Scaling(downscalingBounds, ToHeight).apply(portraitFormatImage)
 
           resized.getWidth shouldBe 30
           resized.getHeight shouldBe 40
         }
 
         "image is in square format." in {
-          val resized = new Scaling(bounds, ToHeight).apply(squareFormatImage)
+          val resized = new Scaling(downscalingBounds, ToHeight).apply(squareFormatImage)
 
           resized.getWidth shouldBe 40
           resized.getHeight shouldBe 40
@@ -89,50 +90,147 @@ class ScalingTest extends WordSpec with Matchers {
       }
     }
 
-    "scale an image to defined bounding box" when {
+    "scale an image down to defined bounding box" when {
       "to bounding box scaling is used" when {
         "image is in landscape format." in {
-          val resized = new Scaling(bounds, ToBoundingBox).apply(landscapeFormatImage)
+          val resized = new Scaling(downscalingBounds, ToBoundingBox).apply(landscapeFormatImage)
 
           resized.getWidth shouldBe 40
           resized.getHeight shouldBe 30
         }
 
         "image is in portrait format." in {
-          val resized = new Scaling(bounds, ToBoundingBox).apply(portraitFormatImage)
+          val resized = new Scaling(downscalingBounds, ToBoundingBox).apply(portraitFormatImage)
 
           resized.getWidth shouldBe 30
           resized.getHeight shouldBe 40
         }
 
         "image is in square format." in {
-          val resized = new Scaling(bounds, ToBoundingBox).apply(squareFormatImage)
+          val resized = new Scaling(downscalingBounds, ToBoundingBox).apply(squareFormatImage)
 
           resized.getWidth shouldBe 40
           resized.getHeight shouldBe 40
         }
       }
     }
+    "scale an image up to defined width" when {
+      "to width scaling is used" when {
+        "image is in landscape format." in {
+          val resized = new Scaling(upscalingBounds, ToWidth).apply(landscapeFormatImage)
 
-    "preserve the image contents." in {
-      val graphics = squareFormatImage.getGraphics.asInstanceOf[Graphics2D]
-      graphics.setColor(Color.WHITE)
-      graphics.fillRect(0, 0, squareFormatImage.getWidth, squareFormatImage.getHeight / 2)
-      graphics.setColor(Color.BLACK)
-      graphics.fillRect(0, squareFormatImage.getHeight / 2 + 1, squareFormatImage.getWidth, squareFormatImage.getHeight / 2)
-      graphics.dispose()
+          resized.getWidth shouldBe 200
+          resized.getHeight shouldBe 150
+        }
 
-      val expectedImage = new BufferedImage(40, 40, BufferedImage.TYPE_BYTE_GRAY)
-      graphics.setColor(Color.WHITE)
-      graphics.fillRect(0, 0, expectedImage.getWidth, expectedImage.getHeight / 2)
-      graphics.setColor(Color.BLACK)
-      graphics.fillRect(0, expectedImage.getHeight / 2 + 1, expectedImage.getWidth, expectedImage.getHeight / 2)
-      graphics.dispose()
+        "image is in portrait format." in {
+          val resized = new Scaling(upscalingBounds, ToWidth).apply(portraitFormatImage)
 
-      val resized = new Scaling(bounds, ToBoundingBox).apply(squareFormatImage)
+          resized.getWidth shouldBe 200
+          resized.getHeight shouldBe 266
+        }
 
-      resized.getRGB(0, 0) shouldBe Color.WHITE.getRGB
-      resized.getRGB(0, 39) shouldBe Color.BLACK.getRGB
+        "image is in square format." in {
+          val resized = new Scaling(upscalingBounds, ToWidth).apply(squareFormatImage)
+
+          resized.getWidth shouldBe 200
+          resized.getHeight shouldBe 200
+        }
+      }
+    }
+
+    "scale an image up to defined height" when {
+      "to height scaling is used" when {
+        "image is in landscape format." in {
+          val resized = new Scaling(upscalingBounds, ToHeight).apply(landscapeFormatImage)
+
+          resized.getWidth shouldBe 266
+          resized.getHeight shouldBe 200
+        }
+
+        "image is in portrait format." in {
+          val resized = new Scaling(upscalingBounds, ToHeight).apply(portraitFormatImage)
+
+          resized.getWidth shouldBe 150
+          resized.getHeight shouldBe 200
+        }
+
+        "image is in square format." in {
+          val resized = new Scaling(upscalingBounds, ToHeight).apply(squareFormatImage)
+
+          resized.getWidth shouldBe 200
+          resized.getHeight shouldBe 200
+        }
+      }
+    }
+
+    "scale an image up to defined bounding box" when {
+      "to bounding box scaling is used" when {
+        "image is in landscape format." in {
+          val resized = new Scaling(upscalingBounds, ToBoundingBox).apply(landscapeFormatImage)
+
+          resized.getWidth shouldBe 200
+          resized.getHeight shouldBe 150
+        }
+
+        "image is in portrait format." in {
+          val resized = new Scaling(upscalingBounds, ToBoundingBox).apply(portraitFormatImage)
+
+          resized.getWidth shouldBe 150
+          resized.getHeight shouldBe 200
+        }
+
+        "image is in square format." in {
+          val resized = new Scaling(upscalingBounds, ToBoundingBox).apply(squareFormatImage)
+
+          resized.getWidth shouldBe 200
+          resized.getHeight shouldBe 200
+        }
+      }
+    }
+
+    "preserve the image contents" when {
+      "downscaled." in {
+        val graphics = squareFormatImage.getGraphics.asInstanceOf[Graphics2D]
+        graphics.setColor(Color.WHITE)
+        graphics.fillRect(0, 0, squareFormatImage.getWidth, squareFormatImage.getHeight / 2)
+        graphics.setColor(Color.BLACK)
+        graphics.fillRect(0, squareFormatImage.getHeight / 2 + 1, squareFormatImage.getWidth, squareFormatImage.getHeight / 2)
+        graphics.dispose()
+
+        val expectedImage = new BufferedImage(40, 40, BufferedImage.TYPE_BYTE_GRAY)
+        graphics.setColor(Color.WHITE)
+        graphics.fillRect(0, 0, expectedImage.getWidth, expectedImage.getHeight / 2)
+        graphics.setColor(Color.BLACK)
+        graphics.fillRect(0, expectedImage.getHeight / 2 + 1, expectedImage.getWidth, expectedImage.getHeight / 2)
+        graphics.dispose()
+
+        val resized = new Scaling(downscalingBounds, ToBoundingBox).apply(squareFormatImage)
+
+        resized.getRGB(0, 0) shouldBe Color.WHITE.getRGB
+        resized.getRGB(0, 39) shouldBe Color.BLACK.getRGB
+      }
+
+      "upscaled." in {
+        val graphics = squareFormatImage.getGraphics.asInstanceOf[Graphics2D]
+        graphics.setColor(Color.WHITE)
+        graphics.fillRect(0, 0, squareFormatImage.getWidth, squareFormatImage.getHeight / 2)
+        graphics.setColor(Color.BLACK)
+        graphics.fillRect(0, squareFormatImage.getHeight / 2 + 1, squareFormatImage.getWidth, squareFormatImage.getHeight / 2)
+        graphics.dispose()
+
+        val expectedImage = new BufferedImage(200, 200, BufferedImage.TYPE_BYTE_GRAY)
+        graphics.setColor(Color.WHITE)
+        graphics.fillRect(0, 0, expectedImage.getWidth, expectedImage.getHeight / 2)
+        graphics.setColor(Color.BLACK)
+        graphics.fillRect(0, expectedImage.getHeight / 2 + 1, expectedImage.getWidth, expectedImage.getHeight / 2)
+        graphics.dispose()
+
+        val resized = new Scaling(upscalingBounds, ToBoundingBox).apply(squareFormatImage)
+
+        resized.getRGB(0, 0) shouldBe Color.WHITE.getRGB
+        resized.getRGB(0, 199) shouldBe Color.BLACK.getRGB
+      }
     }
   }
 }
