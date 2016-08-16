96f91c5cf626181cb9acc26c86d2be3828c8a87e
Daniel Goetten <daniel.goetten@googlemail.com>
2016-08-16 22:14:01 +0200

    Tests for pdfs with content added.

--- imagence-pdf/src/test/scala/de/dangoe/imagence/pdf/PdfConverterTest.scala
+++ imagence-pdf/src/test/scala/de/dangoe/imagence/pdf/PdfConverterTest.scala
@@ -22,7 +22,8 @@ package de.dangoe.imagence.pdf
 
 import java.awt.Color
 import java.awt.image.BufferedImage
-import java.io.ByteArrayInputStream
+import java.io.{ByteArrayInputStream, File}
+import javax.imageio.ImageIO
 
 import de.dangoe.imagence.testsupport._
 import de.dangoe.imagence.api.Implicits._
@@ -34,7 +35,7 @@ import org.scalatest.{Matchers, WordSpec}
   * @author Daniel Götten <daniel.goetten@googlemail.com>
   * @since 12.08.16
   */
-class PdfConverterTest extends WordSpec with Matchers with ImageFactory {
+class PdfConverterTest extends WordSpec with Matchers with ImageFactory with ImageReader {
 
   "The PdfConverter" should {
     "throw an PdfConversionFailed exception" when {
@@ -78,7 +79,7 @@ class PdfConverterTest extends WordSpec with Matchers with ImageFactory {
       }
     }
 
-    "return a sequence of white images" when {
+    "return a sequence of two white images" when {
       "an empty document with two pages is converted." in {
         val sut = new PdfConverter
         val convertedDocument = sut.convert(getClass.getResourceAsStream("empty_din_a4_pdf_with_two_pages.pdf"))
@@ -86,6 +87,29 @@ class PdfConverterTest extends WordSpec with Matchers with ImageFactory {
         convertedDocument.pageCount shouldBe 2
         convertedDocument.foreach { pageAsImage =>
           pageAsImage should showTheSameAs(createImage(Dimension(2479, 3508), Fill(Color.WHITE), BufferedImage.TYPE_INT_RGB))
+        }
+      }
+    }
+
+    "return a image of the document contents" when {
+      "a document with 300 DPI is converted." in {
+        val sut = new PdfConverter(PdfConverterConfiguration.default.withDpi(50))
+        val convertedDocument = sut.convert(getClass.getResourceAsStream("din_a4_pdf_with_one_page.pdf"))
+
+        convertedDocument.pageCount shouldBe 1
+
+        convertedDocument.head should showTheSameAs(readImage("din_a4_pdf_with_one_page-1.png"))
+      }
+    }
+
+    "return a sequence of two images of the document contents" when {
+      "a document with two pages is converted." in {
+        val sut = new PdfConverter(PdfConverterConfiguration.default.withDpi(50))
+        val convertedDocument = sut.convert(getClass.getResourceAsStream("din_a4_pdf_with_two_pages.pdf"))
+
+        convertedDocument.pageCount shouldBe 2
+        (1 to convertedDocument.pageCount).foreach { pageNumber =>
+          convertedDocument.page(pageNumber - 1) should showTheSameAs(readImage(s"din_a4_pdf_with_two_pages-$pageNumber.png"))
         }
       }
     }
